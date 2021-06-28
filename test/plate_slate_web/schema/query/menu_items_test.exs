abfaf09644358925e9fde6368da5174abfb347a9
nallwhy <nallwhy@gmail.com>
2021-06-28 15:10:52 +0900

    Create PlateSlateWeb.Schema.Query.MenuItemsTest

---
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -0,0 +1,40 @@
+defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
+  use PlateSlateWeb.ConnCase, async: true
+
+  setup do
+    PlateSlate.Seeds.run()
+  end
+
+  @query """
+  {
+    menuItems {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns menu items" do
+    conn = get(build_conn(), "/api", query: @query)
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"},
+                 %{"name" => "Croque Monsieur"},
+                 %{"name" => "Muffuletta"},
+                 %{"name" => "Bánh mì"},
+                 %{"name" => "Vada Pav"},
+                 %{"name" => "French Fries"},
+                 %{"name" => "Papadum"},
+                 %{"name" => "Pasta Salad"},
+                 %{"name" => "Water"},
+                 %{"name" => "Soft Drink"},
+                 %{"name" => "Lemonade"},
+                 %{"name" => "Masala Chai"},
+                 %{"name" => "Vanilla Milkshake"},
+                 %{"name" => "Chocolate Milkshake"}
+               ]
+             }
+           }
+  end
+end
