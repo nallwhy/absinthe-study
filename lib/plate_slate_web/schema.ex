291276c8af53d2e6bdec3e436bef5299ef16503c
nallwhy <nallwhy@gmail.com>
2021-06-28 13:57:11 +0900

    Add menu_items to query of GraphQL schema

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,7 +1,13 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
+  alias PlateSlate.Menu
+  alias PlateSlate.Repo
 
   query do
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      resolve fn _, _, _ -> {:ok, Repo.all(Menu.Item)} end
+    end
   end
 
   object :menu_item do
