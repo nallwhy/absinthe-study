424e0ef39ffa86e5e23a3661ec0adecca41a5154
nallwhy <nallwhy@gmail.com>
2021-06-30 17:10:05 +0900

    Add absinthe_phoenix to deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PlateSlate.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.6"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe_phoenix, "~> 2.0"}
     ]
   end
 
